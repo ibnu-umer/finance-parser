215e6a86e3555174aa95ba1ad52224f7e25c2e25
Muhammad Riyas <muhammadriyask11@gmail.com>
2025-10-23 23:33:52 +0530

    refactor(gpay_parser): update gpay_parser logic and create seperate module

--- finance-parser/main.py
+++ finance-parser/main.py
@@ -1,8 +1,20 @@
-from parsers import parse_gpay, parse_canara
-from cleaners import clean_transactions
-from utils import save_csv, save_json, ensure_dir
+import logging, re
+from gpay_parser import gpay_parser
+
+# Supperss `gray color warning`s
+logging.getLogger("pdfminer").setLevel(logging.ERROR)
+
+
+
+def main():
+    pdf_path = "media/gpay_statement.pdf"
+    gpay_df = gpay_parser(pdf_path)
+    gpay_df.to_csv(f"output/{pdf_path.split("/")[-1].replace(".pdf", "")}_result.csv", index=False, encoding="utf-8-sig")
 
 
 
 
 
+
+if __name__ == "__main__":
+    main()
